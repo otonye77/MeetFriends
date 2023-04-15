29ae3a731bfa613d04c3da0af6ecd034f65d0d99
otonye77 <otonye.amietubodie@decagon.dev>
2023-04-15 17:02:14 +0100

    master

--- server/server.js
+++ server/server.js
@@ -12,6 +12,14 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Server is listening on Port ${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server is listening on Port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Database connection failed.");
+    console.log(err);
+  });
