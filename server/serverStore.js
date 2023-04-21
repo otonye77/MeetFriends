4076bb8963b39a3eed1dccd963784e3fb1c6bb9c
otonye77 <oamietubodie@gmail.com>
2023-04-21 12:45:08 +0100

    master

--- server/serverStore.js
+++ server/serverStore.js
@@ -2,9 +2,17 @@ const connectedUser = new Map();
 
 const addNewConnectedUser = ({ socketId, userId }) => {
   connectedUser.set(socketId, { userId });
-  console.log({"new connecteduser": connectedUser});
+  console.log({ "new connecteduser": connectedUser });
+};
+
+const removeConnectedUser = (socketId) => {
+  if (connectedUser.has(socketId)) {
+    connectedUser.delete(socketId);
+    console.log({"new connected user": connectedUser})
+  }
 };
 
 module.exports = {
-    addNewConnectedUser
-}
+  addNewConnectedUser,
+  removeConnectedUser,
+};
