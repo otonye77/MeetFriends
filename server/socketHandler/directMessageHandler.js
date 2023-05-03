3c0317e71a4db4e025ab6bba0c825c2bcd8fc08f
otonye77 <oamietubodie@gmail.com>
2023-05-03 15:07:15 +0100

    master

--- server/socketHandler/directMessageHandler.js
+++ server/socketHandler/directMessageHandler.js
@@ -1,7 +1,31 @@
+const Conversation = require("../models/conversation");
+const Message = require("../models/message");
+
 const directMessageHandler = async (socket, data) => {
   try {
     const { userId } = socket.user;
     const { receiverUserId, content } = data;
+    const message = await Message.create({
+      content: content,
+      authorId: userId,
+      date: new Date(),
+      type: "DIRECT",
+    });
+    //FIND IF CONVERSATION EXIST BETWEEN THIS TWO USERS IF NOT CREATE NEW
+    const conversation = await Conversation.findOne({
+      participants: {
+        $all: [userId, receiverUserId],
+      },
+    });
+    if (conversation) {
+      conversation.messages.push(message._id);
+      await conversation.save()
+    } else {
+      const newConversation = await Conversation.create({
+        messages: [message._id],
+        participants: [userId, receiverUserId]
+      })
+    }
   } catch (err) {
     console.log(err);
   }
