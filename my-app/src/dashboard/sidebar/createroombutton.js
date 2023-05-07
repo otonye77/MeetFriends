27be813d83ba647c08452da2472b75c4ef3bfb6b
otonye77 <oamietubodie@gmail.com>
2023-05-07 12:44:11 +0100

    master

--- my-app/src/dashboard/sidebar/createroombutton.js
+++ my-app/src/dashboard/sidebar/createroombutton.js
@@ -2,25 +2,26 @@ import { Button } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 
 const CreateRoomButton = () => {
-    const createRoomHandler = () => {
-
-    }
-  return <div>
-     <Button
-      style={{
-        width: "48px",
-        height: "48px",
-        borderRadius: "16px",
-        margin: 0,
-        padding: 0,
-        minWidth: 0,
-        marginTop: "10px",
-        color: "white",
-        backgroundColor: "#5865F2",
-      }}
-    >
-      <AddIcon />
-    </Button>
-  </div>;
+  const createRoomHandler = () => {};
+  return (
+    <div>
+      <Button
+        onClick={createRoomHandler}
+        style={{
+          width: "48px",
+          height: "48px",
+          borderRadius: "16px",
+          margin: 0,
+          padding: 0,
+          minWidth: 0,
+          marginTop: "10px",
+          color: "white",
+          backgroundColor: "#5865F2",
+        }}
+      >
+        <AddIcon />
+      </Button>
+    </div>
+  );
 };
 export default CreateRoomButton;
