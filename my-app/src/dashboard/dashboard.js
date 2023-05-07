fadcee9d5897836c67a45969cead64ea3c296652
otonye77 <oamietubodie@gmail.com>
2023-05-07 13:23:00 +0100

    master

--- my-app/src/dashboard/dashboard.js
+++ my-app/src/dashboard/dashboard.js
@@ -5,9 +5,10 @@ import FriendsSidebar from "./friendsSidebar/friendsSidebar";
 import Messenger from "./messenger/messenger";
 import AppBar from "./appbar/appbar";
 import { logout } from "../shared/utils/auth";
-import {connect} from"react-redux";
+import { connect } from "react-redux";
 import { getActions } from "../store/actions/authAction";
 import { connectWithSocketServer } from "../realtimecommunication/socketConnection";
+import Room from "./room/room";
 
 const Wrapper = styled("div")({
   width: "100%",
@@ -15,30 +16,37 @@ const Wrapper = styled("div")({
   display: "flex",
 });
 
-const DashBoardPage = ({setUserDetails}) => {
+const DashBoardPage = ({ setUserDetails, isUserInRoom }) => {
   useEffect(() => {
     const userDetails = localStorage.getItem("user");
-    if(!userDetails){
-      logout()
+    if (!userDetails) {
+      logout();
     } else {
-      setUserDetails(JSON.parse(userDetails))
-      connectWithSocketServer(JSON.parse(userDetails))
+      setUserDetails(JSON.parse(userDetails));
+      connectWithSocketServer(JSON.parse(userDetails));
     }
-  }, [])
+  }, []);
   return (
     <Wrapper>
-        <Sidebar />
-        <FriendsSidebar />
-        <Messenger />
-        <AppBar />
+      <Sidebar />
+      <FriendsSidebar />
+      <Messenger />
+      <AppBar />
+      {isUserInRoom && <Room />}
     </Wrapper>
   );
 };
 
+const mapStoreStateToProps = ({ room }) => {
+  return {
+    ...room,
+  };
+};
+
 const mapActionToProps = (dispatch) => {
   return {
-    ...getActions(dispatch)
-  }
-}
+    ...getActions(dispatch),
+  };
+};
 
-export default connect(null, mapActionToProps)(DashBoardPage);
+export default connect(mapStoreStateToProps, mapActionToProps)(DashBoardPage);
